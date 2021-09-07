a3396770816aa5578ef72ab87c66723bb419a7a0
Joschua Becker <jb@scolasti.co>
2021-09-07 15:52:57 +0200

    added json special pages

---
+++ src/main/java/me/scolastico/tools/web/special/pages/json/InternalError.java
@@ -0,0 +1,30 @@
+package me.scolastico.tools.web.special.pages.json;
+
+import com.sun.net.httpserver.HttpExchange;
+import java.util.HashMap;
+import java.util.List;
+import me.scolastico.tools.pairs.Pair;
+import me.scolastico.tools.web.annoations.WebServerRegistration;
+import me.scolastico.tools.web.enums.SpecialWebsite;
+import me.scolastico.tools.web.interfaces.SimpleWebsiteInterface;
+import org.apache.commons.fileupload.FileItem;
+
+/**
+ * Special page for json instead of plain text. Can be used with:
+ * <code>
+ *   WebServer.registerAllWebInterfacesInPackage("me.scolastico.tools.web.special.pages.json");
+ * </code>
+ */
+public class InternalError implements SimpleWebsiteInterface {
+
+  @Override
+  @WebServerRegistration(
+      context = "",
+      website = SpecialWebsite.INTERNAL_ERROR_PAGE
+  )
+  public Pair<Integer, String> handleRequest(HttpExchange httpExchange, String[] strings, HashMap<String, String> hashMap, HashMap<String, String> hashMap1, String s, List<FileItem> list, Object o) {
+    httpExchange.getResponseHeaders().add("Content-Type", "application/json");
+    return new Pair<>(500, "{\"status\":\"error\",\"error\":\"internal error\"}");
+  }
+
+}
