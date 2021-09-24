43f9d5e68602c13f4490c3e77fe2d3c6e1cef84d
Joschua Becker <jb@scolasti.co>
2021-09-25 00:27:29 +0200

    added custom cookie name to token manager

--- src/main/java/me/scolastico/tools/web/TokenManager.java
+++ src/main/java/me/scolastico/tools/web/TokenManager.java
@@ -7,6 +7,9 @@ import com.sun.net.httpserver.HttpExchange;
  */
 public class TokenManager {
 
+  private final HttpExchange exchange;
+  private final String cookieName;
+
   /**
    * <pre>
    * Get the login token from a http exchange.
@@ -20,17 +23,7 @@ public class TokenManager {
    * @return The login token if found else NULL.
    */
   public static String getLoginToken(HttpExchange exchange) {
-    String token = null;
-    try {
-      if (exchange.getRequestHeaders().containsKey("Authorization")) {
-        token = exchange.getRequestHeaders().getFirst("Authorization");
-        if (token.startsWith("Bearer ")) token = token.substring(7);
-      }
-      if (token == null) {
-        token = CookieManager.getCookie(exchange, "j_session_auth");
-      }
-    } catch (Exception ignored) {}
-    return token;
+    return new TokenManager(exchange, "j_auth_token").getLoginToken();
   }
 
   /**
@@ -39,17 +32,64 @@ public class TokenManager {
    * @param token The token which should be set in the login cookie.
    */
   public static void setLoginToken(HttpExchange exchange, String token) {
-    setLoginToken(exchange, token, 86400L);
+    new TokenManager(exchange, "j_auth_token").setLoginToken(token);
   }
 
   /**
-   * Function to set a login cookie which is valid for one day.
+   * Function to set a login cookie.
    * @param exchange The http exchange where to set the login cookie.
    * @param token The token which should be set in the login cookie.
    * @param validUntil Valid until defines how much seconds should the login cookie be valid.
    */
   public static void setLoginToken(HttpExchange exchange, String token, Long validUntil) {
-    exchange.getResponseHeaders().set("Set-Cookie", "j_session_auth=" + token + "; path=/; Max-Age=" + validUntil);
+    new TokenManager(exchange, "j_auth_token").setLoginToken(token, validUntil);
+  }
+
+  public TokenManager(HttpExchange exchange, String cookieName) {
+    this.exchange = exchange;
+    this.cookieName = cookieName;
+  }
+
+  /**
+   * <pre>
+   * Get the login token from a http exchange.
+   * The login token can be in a...
+   *  - Cookie (set the name with the constructor.
+   *  - "Authorization" header with or without the "Bearer" tag.
+   *  If multiple tokens are set the first one which is found will be returned.
+   *  The order for the checks is the same as above.
+   * </pre>
+   * @return The login token if found else NULL.
+   */
+  public String getLoginToken() {
+    String token = null;
+    try {
+      if (exchange.getRequestHeaders().containsKey("Authorization")) {
+        token = exchange.getRequestHeaders().getFirst("Authorization");
+        if (token.startsWith("Bearer ")) token = token.substring(7);
+      }
+      if (token == null) {
+        token = CookieManager.getCookie(exchange, cookieName);
+      }
+    } catch (Exception ignored) {}
+    return token;
+  }
+
+  /**
+   * Function to set a login cookie which is valid for one day.
+   * @param token The token which should be set in the login cookie.
+   */
+  public void setLoginToken(String token) {
+    setLoginToken(exchange, token, 86400L);
+  }
+
+  /**
+   * Function to set a login cookie.
+   * @param token The token which should be set in the login cookie.
+   * @param validUntil Valid until defines how much seconds should the login cookie be valid.
+   */
+  public void setLoginToken(String token, Long validUntil) {
+    exchange.getResponseHeaders().set("Set-Cookie", cookieName + "=" + token + "; path=/; Max-Age=" + validUntil);
   }
 
 }
