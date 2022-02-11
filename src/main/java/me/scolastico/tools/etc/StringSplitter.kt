d9ed4569bec73a97c805f90afa957ef2fb68b81e
Joschua Becker <jb@scolasti.co>
2022-02-11 22:21:03 +0100

    fix detekt

--- src/main/java/me/scolastico/tools/etc/StringSplitter.kt
+++ src/main/java/me/scolastico/tools/etc/StringSplitter.kt
@@ -1,8 +1,17 @@
 package me.scolastico.tools.etc
 
+/**
+ * Split strings by length into an array.
+ */
 class StringSplitter private constructor() {
     companion object {
 
+        /**
+         * Split strings by length into an array.
+         * @param string The string to split.
+         * @param length The max length after which to split the string.
+         * @return An array of the strings.
+         */
         fun splitStringByLength(string: String, length: Int):Array<String> {
             var remaining = string
             val list = ArrayList<String>()
