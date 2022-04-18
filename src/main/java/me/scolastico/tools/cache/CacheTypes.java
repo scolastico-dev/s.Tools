42534812d2223fcdebcebb5864b78d384c377d1c
Joschua Becker <jb@scolasti.co>
2022-04-18 17:07:25 +0200

    remove cache module

--- src/main/java/me/scolastico/tools/cache/CacheTypes.java
+++
@@ -1,8 +0,0 @@
-package me.scolastico.tools.cache;
-
-/**
- * Internal CacheTypes enum for storing data of the cache.
- */
-public enum CacheTypes {
-  OBJECT, STRING, INTEGER, FLOAT, DOUBLE, LONG, BOOLEAN
-}
